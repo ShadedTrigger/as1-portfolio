e803d5c9e592765a6a43ad5470f663e02cf49a55
Sean <seanjasminsnelson@gmail.com>
2025-10-02 20:35:51 -0400

    Finished Home and About pages

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom'
+
 export default function Home() {
     return (
         <>
-            <p>Hello World!</p>
+            <h1>Welcome!</h1>
+            <h2>Thanks for visiting my Portfolio!</h2>
+            <p>Feel free to click any of the buttons below to find out more about me.</p>
+            <div className="homeLinks">
+                <Link to ="/about">About Me</Link>
+                <Link to ="/projects"> My Projects</Link>
+            </div>
+
+            <h1 className="missionStatement">Mission Statement</h1>
+            <h3>To always be inquisitive, and never stop learning.</h3>
         </>
     )
 }
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Layout() {
     return (
         <nav>
+            <img src="./src/assets/logo.png" alt="logo" className="logo" width={50}/>
+            <p>Sean's Portfoilo</p>
+            <div className="nav-links">
             <Link to ="/">Home</Link>
             <Link to ="/about">About</Link>
             <Link to ="/projects">Projects</Link>
             <Link to ="/services">Services</Link>
             <Link to ="/contact">Contact</Link>
+            </div>
         </nav>
     )
 }
