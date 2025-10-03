28728bc86eba7f11038bc8406dffe69fbb929d5a
Sean <seanjasminsnelson@gmail.com>
2025-10-03 12:31:57 -0400

    Moved images to /public folder

--- src/Project1.jsx
+++ src/Project1.jsx
@@ -5,7 +5,7 @@ export default function Project1() {
         <>
             <h1>Project 1: Bug Smasher Game</h1>
             <div className="projectsDescriptionContainer">
-                <img src="./src/assets/project1.png" alt="Screenshot of my Bug Smasher Game" className="project1Image"/>
+                <img src="./public/project1.png" alt="Screenshot of my Bug Smasher Game" className="project1Image"/>
                 <p className="aboutMe">This project is a game where you smash bugs created for my Client-Side Web Development class! This is a simple game where you click on the bug to smash it, and it speeds up over time.</p>
             </div>
             <div className="buttons">
